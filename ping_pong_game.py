f5cd28fd4b0ee07acf6691f3cb46bdd73d99492d
crazybear789 <84227355+crazybear789@users.noreply.github.com>
2021-05-22 12:11:29 +0300

    Update ping_pong_game.py

--- ping_pong_game.py
+++ ping_pong_game.py
@@ -1,1 +1,11 @@
 import pygame
+window = display.set_mode((700, 500))
+display.set_caption("Ping-Pong")
+#background = transform.scale(image.load(''), (700, 500))
+
+game = True 
+while game:
+    for e in event.get():
+        if e.tipe == QUIT:
+            game = False
+    display.update() 
