514f2a5254f1e8393d88e06d4471eef9ec8d1c4f
crazybear789 <84227355+crazybear789@users.noreply.github.com>
2021-05-22 12:41:45 +0300

    Update ping_pong_game.py

--- ping_pong_game.py
+++ ping_pong_game.py
@@ -2,10 +2,15 @@ import pygame
 window = display.set_mode((700, 500))
 display.set_caption("Ping-Pong")
 #background = transform.scale(image.load(''), (700, 500))
-
+#window.feel('blue')
+clock = time.Clock()
+FPS = 60
 game = True 
 while game:
+  
     for e in event.get():
         if e.tipe == QUIT:
             game = False
     display.update() 
+    
+clock.tick(FPS)
